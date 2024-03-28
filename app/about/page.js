da9e7591fa2bc7c566c2747cd1d649b9c7dbdb22
outro1111 <outro1111@naver.com>
2024-03-28 21:09:27 +0900

    프리패치 테스트

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,6 @@
 import Search from "../components/Search";
 
-export default function Home({ searchParams }) {
+export default function AboutPage({ searchParams }) {
   return (
     <>
       <h1>Live Params</h1>
--- app/movies/page.js
+++ app/movies/page.js
@@ -1,11 +1,21 @@
 import MovieList from '../components/MovieList'
 import Search from "../components/Search";
 
-export default function MoviesPage({ searchParams: {title} }) {
+export default async function MoviesPage({ searchParams: {title} }) {
+
+  let url = `${process.env.API_URL}/api/movies?populate=*&sort=createdAt%3Adesc`;
+  if (title) {
+    url += `&filters[$or][0][title][$containsi]=${title}&filters[$or][1][titleOriginal][$containsi]=${title}`;
+  }
+  const res = await fetch(url, {
+    cache: "no-store"
+  })
+  const json = await res.json()
+  const movieList = json.data
   return (
     <>
       <Search />
-      <MovieList title={title} />
+      <MovieList movieList={movieList} />
     </>
   );
 }
