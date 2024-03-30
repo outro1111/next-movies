7715ac598ad8a91bfdac162500e61d807e267558
outro1111 <outro1111@naver.com>
2024-03-30 21:17:17 +0900

    프리패치 테스트

--- app/movies/[id]/page.js
+++ app/movies/[id]/page.js
@@ -5,8 +5,6 @@ import Markdown from "react-markdown"
 
 // 상세 정보 불러오기
 const getMovieDetail = async (id) => {
-  // await new Promise((resolve) => setTimeout(resolve, 2000))
-  // const res = await fetch(`https://strapi-movies-production.up.railway.app/api/movies/${id}?populate[0]=poster&populate[1]=cast.photo&populate[2]=image`)
   try {
     const res = await fetch(`${process.env.API_URL}/api/movies/${id}?populate[0]=poster&populate[1]=cast.photo&populate[2]=image`);
     const json = await res.json()
